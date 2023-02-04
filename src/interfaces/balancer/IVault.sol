5a8a842112c4d0d87cf4fdb1a2990b797498851a
GUILHERME NEVES <asguilift@hotmail.com>
2023-02-04 15:03:45 -0300

    add: identation wars: part 5

--- src/interfaces/balancer/IVault.sol
+++ src/interfaces/balancer/IVault.sol
@@ -1,60 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 pragma experimental ABIEncoderV2;
+pragma solidity >=0.7.0 <0.9.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AztecTypes} from "rollup-encoder/libraries/AztecTypes.sol";
 
-pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * @dev This interface was originally fetched from balancer-v2-monorepo and editted to suit
- *      what the Aztec protocol needs to communicate with the Balancer Bridge
- */
 interface IAsset {
 // solhint-disable-previous-line no-empty-blocks
 }
 interface IVault {
-    /// @dev Returns the poolId for this pool
-    /// @return The poolId for this pool
-    function getPoolId() external view returns (bytes32);
-    
-    enum JoinKind { 
-        INIT, 
-        EXACT_TOKENS_IN_FOR_BPT_OUT, 
-        TOKEN_IN_FOR_EXACT_BPT_OUT, 
-        ALL_TOKENS_IN_FOR_EXACT_BPT_OUT 
-    }
-
-    enum ExitKind { 
-        EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, 
-        EXACT_BPT_IN_FOR_TOKENS_OUT, 
-        BPT_IN_FOR_EXACT_TOKENS_OUT 
-    }
+    /** 
+     * @dev Interfaces below are elements known to Aztec and Balancer and
+     * were arranged in a strategical manner for the bridge to operate.
+     */
 
     /**
-     * @dev Custom struct created to aid when calling 
-     *      convert function in the BalancerBridge contract. 
-     */ 
-    enum ActionKind {
-        JOIN,
-        EXIT,
-        SWAP,
-        BATCHSWAP
-    }
-
+     * @dev Usins struct so that the convert(...) function won't dive into stackTooDeep error.
+     */
     struct Convert {
         AztecTypes.AztecAsset inputAssetA;
         AztecTypes.AztecAsset inputAssetB;
@@ -85,6 +48,7 @@ interface IVault {
         uint256 limit;
         uint256 deadline;
     }
+
     struct BatchSwap {
         SwapKind kind;
         BatchSwapStep[] swaps;
@@ -94,6 +58,11 @@ interface IVault {
         uint256 deadline;
     }
 
+    /**
+     * @dev This swap is from the typescript interface when calulating batchswaps off-chain.
+     * 
+     * Froundry needs to be solidity compatible for most cases and this had to replicated here.
+     */
     struct Trade {
         bytes32 poolId;
         address tokenIn;
@@ -101,7 +70,28 @@ interface IVault {
         uint256 amount;
     }
 
-    /* end of custom interfaces */
+    /**
+     * @dev Sort the type of actions available in the Vault.
+     */
+    enum ActionKind {
+        JOIN,
+        EXIT,
+        SWAP,
+        BATCHSWAP
+    }
+
+    enum JoinKind { 
+        INIT, 
+        EXACT_TOKENS_IN_FOR_BPT_OUT, 
+        TOKEN_IN_FOR_EXACT_BPT_OUT, 
+        ALL_TOKENS_IN_FOR_EXACT_BPT_OUT 
+    }
+
+    enum ExitKind { 
+        EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, 
+        EXACT_BPT_IN_FOR_TOKENS_OUT, 
+        BPT_IN_FOR_EXACT_TOKENS_OUT 
+    }
 
     enum PoolSpecialization { GENERAL, MINIMAL_SWAP_INFO, TWO_TOKEN }
 
@@ -109,6 +99,12 @@ interface IVault {
      * @dev Returns a Pool's contract address and specialization setting.
      */
     function getPool(bytes32 poolId) external view returns (address, PoolSpecialization);
+
+    /**
+     * @dev Returns a Pool's unique identifier.
+     */
+    function getPoolId() external view returns (bytes32);
+
 
     /**
      * @dev Returns a Pool's registered tokens, the total balance for each, and the latest block when *any* of
@@ -230,53 +226,52 @@ interface IVault {
 
     enum PoolBalanceChangeKind { JOIN, EXIT }
 
-    // Swaps
-    //
-    // Users can swap tokens with Pools by calling the `swap` and `batchSwap` functions. To do this,
-    // they need not trust Pool contracts in any way: all security checks are made by the Vault. They must however be
-    // aware of the Pools' pricing algorithms in order to estimate the prices Pools will quote.
-    //
-    // The `swap` function executes a single swap, while `batchSwap` can perform multiple swaps in sequence.
-    // In each individual swap, tokens of one kind are sent from the sender to the Pool (this is the 'token in'),
-    // and tokens of another kind are sent from the Pool to the recipient in exchange (this is the 'token out').
-    // More complex swaps, such as one token in to multiple tokens out can be achieved by batching together
-    // individual swaps.
-    //
-    // There are two swap kinds:
-    //  - 'given in' swaps, where the amount of tokens in (sent to the Pool) is known, and the Pool determines (via the
-    // `onSwap` hook) the amount of tokens out (to send to the recipient).
-    //  - 'given out' swaps, where the amount of tokens out (received from the Pool) is known, and the Pool determines
-    // (via the `onSwap` hook) the amount of tokens in (to receive from the sender).
-    //
-    // Additionally, it is possible to chain swaps using a placeholder input amount, which the Vault replaces with
-    // the calculated output of the previous swap. If the previous swap was 'given in', this will be the calculated
-    // tokenOut amount. If the previous swap was 'given out', it will use the calculated tokenIn amount. These extended
-    // swaps are known as 'multihop' swaps, since they 'hop' through a number of intermediate tokens before arriving at
-    // the final intended token.
-    //
-    // In all cases, tokens are only transferred in and out of the Vault (or withdrawn from and deposited into Internal
-    // Balance) after all individual swaps have been completed, and the net token balance change computed. This makes
-    // certain swap patterns, such as multihops, or swaps that interact with the same token pair in multiple Pools, cost
-    // much less gas than they would otherwise.
-    //
-    // It also means that under certain conditions it is possible to perform arbitrage by swapping with multiple
-    // Pools in a way that results in net token movement out of the Vault (profit), with no tokens being sent in (only
-    // updating the Pool's internal accounting).
-    //
-    // To protect users from front-running or the market changing rapidly, they supply a list of 'limits' for each token
-    // involved in the swap, where either the maximum number of tokens to send (by passing a positive value) or the
-    // minimum amount of tokens to receive (by passing a negative value) is specified.
-    //
-    // Additionally, a 'deadline' timestamp can also be provided, forcing the swap to fail if it occurs after
-    // this point in time (e.g. if the transaction failed to be included in a block promptly).
-    //
-    // If interacting with Pools that hold WETH, it is possible to both send and receive ETH directly: the Vault will do
-    // the wrapping and unwrapping. To enable this mechanism, the IAsset sentinel value (the zero address) must be
-    // passed in the `assets` array instead of the WETH address. Note that it is possible to combine ETH and WETH in the
-    // same swap. Any excess ETH will be sent back to the caller (not the sender, which is relevant for relayers).
-    //
-    // Finally, Internal Balance can be used when either sending or receiving tokens.
-
+    /** Swaps
+     * @dev Users can swap tokens with Pools by calling the `swap` and `batchSwap` functions. To do this,
+     * they need not trust Pool contracts in any way: all security checks are made by the Vault. They must however be
+     * aware of the Pools' pricing algorithms in order to estimate the prices Pools will quote.
+     *
+     * The `swap` function executes a single swap, while `batchSwap` can perform multiple swaps in sequence.
+     * In each individual swap, tokens of one kind are sent from the sender to the Pool (this is the 'token in'),
+     * and tokens of another kind are sent from the Pool to the recipient in exchange (this is the 'token out').
+     * More complex swaps, such as one token in to multiple tokens out can be achieved by batching together
+     * individual swaps.
+     *
+     * There are two swap kinds:
+     *  - 'given in' swaps, where the amount of tokens in (sent to the Pool) is known, and the Pool determines (via the
+     * `onSwap` hook) the amount of tokens out (to send to the recipient).
+     *  - 'given out' swaps, where the amount of tokens out (received from the Pool) is known, and the Pool determines
+     * (via the `onSwap` hook) the amount of tokens in (to receive from the sender).
+     *
+     * Additionally, it is possible to chain swaps using a placeholder input amount, which the Vault replaces with
+     * the calculated output of the previous swap. If the previous swap was 'given in', this will be the calculated
+     * tokenOut amount. If the previous swap was 'given out', it will use the calculated tokenIn amount. These extended
+     * swaps are known as 'multihop' swaps, since they 'hop' through a number of intermediate tokens before arriving at
+     * the final intended token.
+     *
+     * In all cases, tokens are only transferred in and out of the Vault (or withdrawn from and deposited into Internal
+     * Balance) after all individual swaps have been completed, and the net token balance change computed. This makes
+     * certain swap patterns, such as multihops, or swaps that interact with the same token pair in multiple Pools, cost
+     * much less gas than they would otherwise.
+     *
+     * It also means that under certain conditions it is possible to perform arbitrage by swapping with multiple
+     * Pools in a way that results in net token movement out of the Vault (profit), with no tokens being sent in (only
+     * updating the Pool's internal accounting).
+     *
+     * To protect users from front-running or the market changing rapidly, they supply a list of 'limits' for each token
+     * involved in the swap, where either the maximum number of tokens to send (by passing a positive value) or the
+     * minimum amount of tokens to receive (by passing a negative value) is specified.
+     *
+     * Additionally, a 'deadline' timestamp can also be provided, forcing the swap to fail if it occurs after
+     * this point in time (e.g. if the transaction failed to be included in a block promptly).
+     *
+     * If interacting with Pools that hold WETH, it is possible to both send and receive ETH directly: the Vault will do
+     * the wrapping and unwrapping. To enable this mechanism, the IAsset sentinel value (the zero address) must be
+     * passed in the `assets` array instead of the WETH address. Note that it is possible to combine ETH and WETH in the
+     * same swap. Any excess ETH will be sent back to the caller (not the sender, which is relevant for relayers).
+     *
+     * Finally, Internal Balance can be used when either sending or receiving tokens.
+     */
     enum SwapKind { GIVEN_IN, GIVEN_OUT }
 
     /**
